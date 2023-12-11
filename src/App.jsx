e786673f9e69843c00789c86187ffe2ad16859cd
muslim <bukhori.muslim2210@gmail.com>
2023-12-11 14:13:33 +0700

    membuat productionHouse film background video

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Slider from "./components/Slider";
+import ProductionHouse from "./components/ProductionHouse";
 
 function App() {
   return (
     <div>
       <Header />
       <Slider />
+      <ProductionHouse />
     </div>
   );
 }
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,9 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import GlobalApi from "../services/GlobalApi";
+import { HiChevronLeft, HiChevronRight } from "react-icons/hi2";
 
 const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original/";
+
+const screenWidth = window.innerWidth;
 const Slider = () => {
   const [movieList, setMovieList] = useState([]);
+
+  const elementRef = useRef();
+
   useEffect(() => {
     getTrendingMovies();
   }, []);
@@ -15,15 +21,36 @@ const Slider = () => {
     });
   };
 
+  const sliderRight = (element) => {
+    element.scrollLeft += screenWidth - 110;
+  };
+  const sliderLeft = (element) => {
+    element.scrollLeft -= screenWidth - 110;
+  };
+
   return (
-    <div className="flex overflow-x-auto w-full px-16 py-4">
-      {movieList.map((item) => (
-        <img
-          key={item.id}
-          src={IMAGE_BASE_URL + item.backdrop_path}
-          className="min-w-full h-[310px] object-cover object-left-top mr-5"
-        />
-      ))}
+    <div>
+      <HiChevronLeft
+        className="text-white text-[30px] absolute mx-8 mt-[150px] cursor-pointer hidden md:block"
+        onClick={() => sliderLeft(elementRef.current)}
+      />
+      <HiChevronRight
+        className="text-white text-[30px] absolute mx-8 mt-[150px] cursor-pointer right-0 hidden md:block"
+        onClick={() => sliderRight(elementRef.current)}
+      />
+
+      <div
+        className="flex overflow-x-auto w-full px-16 py-4 scrollbar-none scroll-smooth"
+        ref={elementRef}
+      >
+        {movieList.map((item) => (
+          <img
+            key={item.id}
+            src={IMAGE_BASE_URL + item.backdrop_path}
+            className="min-w-full md:h-[310px] object-cover object-left-top mr-5 rounded-md hover:border-[4px] border-gray-400 transition-all duration-200 ease-in-out"
+          />
+        ))}
+      </div>
     </div>
   );
 };
